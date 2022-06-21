91c14b182067de6a5cc8b7dfe13a6941c64163c5
queirozdaniel <danielqueirozak77@gmail.com>
2022-06-20 21:27:12 -0300

    chore: Initial Project

---
+++ src/main/kotlin/com/danielqueiroz/shoppinggql/ShoppingGqlApplication.kt
@@ -0,0 +1,11 @@
+package com.danielqueiroz.shoppinggql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingGqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShoppingGqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/danielqueiroz/shoppinggql/ShoppingGqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.danielqueiroz.shoppinggql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoppingGqlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
