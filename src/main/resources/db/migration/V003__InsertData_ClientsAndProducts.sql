e5257b3594aaa20c64da0d7ef787cf82c977949f
queirozdaniel <danielqueirozak77@gmail.com>
2022-06-23 23:19:10 -0300

    feat: Created Migrations to insert some data

---
+++ src/main/resources/db/migration/V003__InsertData_ClientsAndProducts.sql
@@ -0,0 +1,11 @@
+INSERT INTO Client (id, email, name) VALUES (1, 'dqz@gmail.com', 'Daniel Queiroz');
+INSERT INTO Client (id, email, name) VALUES (2, 'jh12@gmail.com', 'John V');
+INSERT INTO Client (id, email, name) VALUES (3, 'maah2@gmail.com', 'Maria V');
+
+INSERT INTO Product (id, name, price) VALUES (1, 'Boné', 25);
+INSERT INTO Product (id, name, price) VALUES (2, 'Bola', 120);
+INSERT INTO Product (id, name, price) VALUES (3, 'Bermuda', 60);
+INSERT INTO Product (id, name, price) VALUES (4, 'Camiseta', 80);
+INSERT INTO Product (id, name, price) VALUES (5, 'Chuteira', 130);
+
+
