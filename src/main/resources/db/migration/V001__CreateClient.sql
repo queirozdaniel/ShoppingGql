ddb0c1efbde7ff549535c057492c58bae98ad32e
queirozdaniel <danielqueirozak77@gmail.com>
2022-06-23 23:11:05 -0300

    feat: Created Migrations to Client and Product

---
+++ src/main/resources/db/migration/V001__CreateClient.sql
@@ -0,0 +1,5 @@
+create table Client (
+    id bigint auto_increment primary key,
+    email varchar(255) null,
+    name varchar(255) null
+);
---
+++ src/main/resources/db/migration/V002__CreateProduct.sql
@@ -0,0 +1,5 @@
+create table Product (
+    id bigint auto_increment primary key,
+    name varchar(255) null,
+    price double not null
+);
